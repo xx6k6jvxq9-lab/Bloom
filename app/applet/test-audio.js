3e4988e5f75b54d01dc26be486572b9848dad97a
xx6k6jvxq9-lab <xx6k6jvxq9@privaterelay.appleid.com>
2026-03-12 21:47:02 +0800

    cleanup stable after low risk file removal

--- app/applet/test-audio.js
+++
@@ -1,8 +0,0 @@
-import https from 'https';
-
-https.get('https://music.163.com/song/media/outer/url?id=1973665667.mp3', (res) => {
-  console.log('Status Code:', res.statusCode);
-  console.log('Headers:', res.headers);
-}).on('error', (e) => {
-  console.error(e);
-});
--- test-fetch.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.get('http://localhost:3000/api/netease/playlist?id=3778678', (res) => {
-  let data = '';
-  res.on('data', (chunk) => data += chunk);
-  res.on('end', () => console.log(data.substring(0, 200)));
-});
